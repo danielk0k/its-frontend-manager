eae2ad3551398f71b4aeb726e0221220e09a013c
Janssen <janssenlau88@gmail.com>
2024-02-26 04:34:27 +0800

    Implement auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,6 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={cn(
-          "min-h-screen bg-background font-sans antialiased",
           inter.className
         )}
       >
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,48 @@
-import React from "react";
+"use client"
 
-export default function LoginView() {
-  return <div>LoginPage</div>;
+import React, { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { LoginForm }  from "./auth/LoginForm";
+import { RegisterForm } from "./auth/RegisterForm";
+
+export default function Page() {
+  const [showLoginForm, setShowLoginForm] = useState(true);
+
+  const handleRegisterClick = () => {
+    setShowLoginForm(!showLoginForm);
+  };
+
+  return (
+    <div style={{ display: "flex", height: "100vh" }}>
+      <div style={{ flex: "70%"}}>
+        {/* to add image */}
+      </div>
+      <div style={{ flex: "30%", backgroundColor: "#ccc", padding: "20px" }}>
+        {showLoginForm ? (
+          <>
+          <LoginForm /> 
+          <div style={{ justifyContent: 'space-between', alignItems: 'center', display: 'flex' }}>
+            <div style={{ display: 'flex' }}>
+              <p style={{ color: 'gray' }}> Don't have an account? </p>
+            </div>
+            <div>
+              <Button variant="link" onClick={handleRegisterClick}>Register now</Button>
+            </div>
+          </div> 
+          </>)
+        : (
+          <><RegisterForm />
+            <div style={{ justifyContent: 'space-between', alignItems: 'center', display: 'flex' }}>
+              <div style={{ display: 'flex' }}>
+                <p style={{ color: 'gray' }}> Have an account? </p>
+              </div>
+              <div>
+                <Button variant="link" onClick={handleRegisterClick}>Login now</Button>
+              </div>
+            </div> 
+          </>)
+        }
+      </div>
+    </div>
+  );
 }
