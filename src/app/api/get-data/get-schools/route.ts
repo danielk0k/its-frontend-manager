b4c892418bf14f6cd4ece80f87eafa0cb3ac6ff8
weesiau <siauwee@u.nus.edu>
2024-03-03 20:32:47 +0800

    Set parameter type of callback function

--- src/app/api/get-data/get-schools/route.ts
+++ src/app/api/get-data/get-schools/route.ts
@@ -2,11 +2,16 @@ import { NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
 
 // example usage:
+// interface School {
+//     id: string;
+//     name: string;
+// }
+
 // const res = await fetch(process.env.URL + '/api/get-data/get-schools', {
 //     method: 'GET',
 // });
 // const data = await res.json();
-// const parsed: School[] = data.school_ids.map(school => ({
+// const parsed: School[] = data.school_ids.map((school: School) => ({
 //     id: school.id,
 //     name: school.name,
 // }));
