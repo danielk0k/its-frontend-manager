9dcd4aeb5f699dee14d8ecf39ce33a612c9611d7
LAPTOP-UOT6M2SH\manas <manasijsaha9616@gmail.com>
2024-03-17 21:09:32 +0800

    implement my courses view

---
+++ src/actions/getCourses.ts
@@ -0,0 +1,32 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+
+
+export async function getCourses({ userEmail }: { userEmail: string }) {
+    try {
+      const user = await prisma.user.findUnique({
+        where: {
+          email: userEmail,
+        },
+        include: {
+            created_courses: true,
+            joined_courses: true,
+        },
+      });
+
+      console.log("hello")
+
+      const courseUser = user;
+  
+      if (!user) {
+        return null;
+      }
+      console.log(user)
+      return courseUser;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  }
+  
